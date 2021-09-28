b63955fceb3cba2d4444febcd46adb4cac245670
EdwinLeon98 <edwinleon98@gmail.com>
2021-09-28 10:47:15 -0700

    My first commit

---
+++ Assets/Movement.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour{
+
+    private Rigidbody2D body;
+    float hMove; //Horizontal Movement
+    public float moveSpeed = 10f;
+    public float height = 10f;
+    public bool isJumping = false;
+
+    // Start is called before the first frame update
+    void Start() {
+        body = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update(){
+        hMove = Input.GetAxisRaw("Horizontal");
+        body.velocity = new Vector2(hMove * moveSpeed, body.velocity.y);
+
+        if (Input.GetKeyDown(KeyCode.Space) && !isJumping) {
+            body.velocity = new Vector2(body.velocity.x, height);
+            isJumping = true;
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D col) {
+        if(col.gameObject.tag == "ground") {
+            isJumping = false;
+        }
+    }
+}
